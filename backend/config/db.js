039520283e2ec5725f8ab580eb9f444e4457d0e6
Raunak Pandey <praunak82@gmail.com>
2024-05-02 10:20:29 +0530

    shayad aab ho jaaye

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   mongoose
-    .connect(
-      process.env.MONGO_URI ||
-        "mongodb+srv://raunakpandey:Raunak8585@cluster0.fxjofgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    )
+    .connect(process.env.MONGO_URI)
     .then(() => console.log(`MongoDB Connected`.cyan.underline))
     .catch((err) => {
       console.log(`Error: ${err.message}`.red.bold);
