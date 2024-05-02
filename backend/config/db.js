18b51b4cf651e94a91727bb3d1d743c53e52715a
Raunak Pandey <praunak82@gmail.com>
2024-05-02 09:36:17 +0530

    new test

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-    } catch (error) { 
-        console.log(`Error: ${error.message}`.red.bold)
-        process.exit();
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    });
+    console.log(
+      `MongoDB Connected: ${conn.connection.host} on ${process.env.MONGO_URI}`
+        .cyan.underline
+    );
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
 
 module.exports = connectDB;
